ac1258856a9edc9824bb5986eee16aee769092a9
Isaac <48893799+IsaacZM@users.noreply.github.com>
2022-08-17 02:09:49 +0200

    chore(main): added log.

--- main.py
+++ main.py
@@ -2,14 +2,10 @@ import os
 import requests
 import time
 import json
-import subprocess
 import fgourl
 import user
-
-root_path = os.getcwd()
-asset_bundle_json = os.path.join(root_path, "assetbundle.json")
-asset_bundle_extractor = os.path.join(
-    root_path, "Asset Bundle Extractor", "Asset Bundle Extractor.exe")
+import coloredlogs
+import logging
 
 # Enviroments Variables
 userIds = os.environ['userIds'].split(',')
@@ -26,10 +22,8 @@ userNums = len(userIds)
 authKeyNums = len(authKeys)
 secretKeyNums = len(secretKeys)
 
-
-def get_assets_json(assetbundle):
-    subprocess.run([asset_bundle_extractor, '-a',
-                    assetbundle, '-r', fate_region])
+logger = logging.getLogger("FGO Daily Login")
+coloredlogs.install(fmt='%(asctime)s %(name)s %(levelname)s %(message)s')
 
 
 def get_latest_verCode():
@@ -48,20 +42,23 @@ def get_latest_verCode():
 
 def main():
     if userNums == authKeyNums and userNums == secretKeyNums:
+        logger.info('Getting Lastest Assets Info')
         fgourl.set_latest_assets()
 
         for i in range(userNums):
             try:
                 instance = user.user(userIds[i], authKeys[i], secretKeys[i])
                 time.sleep(3)
+                logger.info('Loggin into account!')
                 instance.topLogin()
                 time.sleep(2)
                 instance.topHome()
                 time.sleep(2)
+                logger.info('Throw daily friend summon!')
                 instance.drawFP()
                 time.sleep(2)
             except Exception as ex:
-                print(f'{i}th user login failed: {ex}')
+                logger.error(ex)
 
 
 if __name__ == "__main__":
