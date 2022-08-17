5a3eae7681f52d8f148cc495878d96ecb09890f6
Isaac <48893799+IsaacZM@users.noreply.github.com>
2022-08-17 10:43:33 +0200

    fix(CatAndMouseGame): Fix region JP

--- CatAndMouseGame.py
+++ CatAndMouseGame.py
@@ -2,14 +2,17 @@ import base64
 import py3rijndael
 import gzip
 import msgpack
+import main
 
 
 def getAssetBundle(assetbundle):
     data = base64.b64decode(assetbundle)
-
-    key = b'nn33CYId2J1ggv0bYDMbYuZ60m4GZt5P'
+    key = b'nn33CYId2J1ggv0bYDMbYuZ60m4GZt5P'  # By default is NA
     iv = data[:32]
     array = data[32:]
+
+    if main.fate_region == "JP":
+        key = b'W0Juh4cFJSYPkebJB9WpswNF51oa6Gm7'
 
     cipher = py3rijndael.RijndaelCbc(
         key,
