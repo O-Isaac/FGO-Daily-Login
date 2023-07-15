07b5f7ab84c3b1bb1fe0ab4e423aee3beff6c934
Isaac <98283822+O-Isaac@users.noreply.github.com>
2023-07-15 10:58:44 +0000

    feat(main): Add cron make apple

--- main.py
+++ main.py
@@ -7,15 +7,20 @@ import user
 import coloredlogs
 import logging
 
-# Enviroments Variables
-userIds = os.environ['userIds'].split(',')
-authKeys = os.environ['authKeys'].split(',')
-secretKeys = os.environ['secretKeys'].split(',')
-fate_region = os.environ['fateRegion']
-webhook_discord_url = os.environ['webhookDiscord']
-UA = os.environ['UserAgent']
+from datetime import datetime
+from croniter import croniter
 
-if UA != 'nullvalue':
+# Enviroments Variables
+userIds = os.environ.get("userIds").split(',')
+authKeys = os.environ.get("authKeys").split(',')
+secretKeys = os.environ.get("secretKeys").split(',')
+fate_region = os.environ.get("fateRegion")
+webhook_discord_url = os.environ.get("webhookDiscord")
+blue_apple_cron = os.environ.get("MAKE_BLUE_APPLE")
+
+UA = os.environ.get("UserAgent")
+
+if UA:
     fgourl.user_agent_ = UA
 
 userNums = len(userIds)
@@ -24,6 +29,19 @@ secretKeyNums = len(secretKeys)
 
 logger = logging.getLogger("FGO Daily Login")
 coloredlogs.install(fmt='%(asctime)s %(name)s %(levelname)s %(message)s')
+
+
+def check_blue_apple_cron(instance):
+    if blue_apple_cron:
+
+        cron = croniter(blue_apple_cron)
+        next_date = cron.get_next(datetime)
+        current_date = datetime.now()
+        
+        if current_date >= next_date:
+            logger.info('Trying buy one blue apple!')
+            instance.buyBlueApple(1)
+            time.sleep(2)
 
 
 def get_latest_verCode():
@@ -56,9 +74,6 @@ def main():
                 time.sleep(2)
                 logger.info('Throw daily friend summon!')
                 instance.drawFP()
-                time.sleep(2)
-                logger.info('Trying buy one blue apple!')
-                instance.buyBlueApple(1)
                 time.sleep(2)
             except Exception as ex:
                 logger.error(ex)
